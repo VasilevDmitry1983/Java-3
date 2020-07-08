02ba4432cb324fbafea956b99f1bb621fe6e5df7
VasilevDmitry1983 <VasilevDmitry11@mail.ru>
2020-07-08 22:24:46 +0400

    ДЗ 5

---
+++ Les5/src/Road.java
@@ -0,0 +1,24 @@
+import java.util.concurrent.Semaphore;
+
+public class Road extends Stage {
+    public Road(int length) {
+        this.length = length;
+        this.description = "Дорога " + length + " метров";
+    }
+    @Override
+    public void go(Car c, Semaphore smp) {
+        try {
+            System.out.println(c.getName() + " начал этап: " + description);
+            Thread.sleep(length / c.getSpeed() * 1000);
+            System.out.println(c.getName() + " закончил этап: " + description);
+
+            if (Main.count == 4 ){
+                System.out.println("Победил " + c.getName());
+            }
+            Main.count++;
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
